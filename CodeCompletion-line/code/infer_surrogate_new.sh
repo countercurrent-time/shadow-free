5ddcbfa200643d5aedc5a3611a0f40ad0e370ab8
countercurrent-time <358935062@qq.com>
2024-12-25 20:40:15 +0800

    update output path in process.py

--- CodeCompletion-line/code/infer_surrogate_new.sh
+++ CodeCompletion-line/code/infer_surrogate_new.sh
@@ -3,10 +3,10 @@
 export CUDA_VISIBLE_DEVICES=2
 export MODE=surrogate
 LANG=java
-# DATADIR=../../CodeCompletion-token/dataset/javaCorpus/token_completion
 SAMPLE_RATIO=20
 Percentage=0.01
 DATADIR="../dataset/javaCorpus/${Percentage}/${SAMPLE_RATIO}/"
+# DATADIR=../../CodeCompletion-token/dataset/javaCorpus/token_completion
 LITFILE=../../CodeCompletion-token/dataset/javaCorpus/literals.json
 OUTPUTDIR=../../CodeCompletion-token/save/javaCorpus
 PRETRAINDIR=../../CodeCompletion-token/save/javaCorpus/microsoft/CodeGPT-small-java/20/checkpoint-epoch-4
